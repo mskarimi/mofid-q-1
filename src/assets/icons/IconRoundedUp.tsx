be223c6b14b951630dfa75f9cad970bdc1118c2f
mohamad sadegh karimi <m.s.karimi.ubuntu@gmail.com>
2022-12-30 00:42:03 +0330

    added icons

---
+++ src/assets/icons/IconRoundedUp.tsx
@@ -0,0 +1,9 @@
+function IconRoundedDown({className}: {className: string}) {
+  return (
+    <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000" fill="currentColor">
+      <path d="m785.7 580.1-251-251c-9.6-9.6-22.1-14.3-34.6-14.3s-25.1 4.8-34.6 14.3l-251 251c-19.1 19.1-19.1 50.1 0 69.3l21.6 21.6c19.1 19.1 50.1 19.1 69.3 0l160.2-160.2c9.6-9.6 22.1-14.3 34.6-14.3s25.1 4.8 34.6 14.3L695 671c19.1 19.1 50.1 19.1 69.3 0l21.6-21.6c18.9-19.2 18.9-50.2-.2-69.3z" />
+    </svg>
+  );
+}
+
+export default IconRoundedDown;
---
+++ src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+import IconRoundedDown from "assets/icons/IconRoundedDown";
+import IconRoundedUp from "assets/icons/IconRoundedUp";
+
+export {IconRoundedDown, IconRoundedUp};
+
+export type TIcons = ({className}: {className: string}) => JSX.Element;
